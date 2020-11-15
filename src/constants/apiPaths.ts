2f8297534f19c9215c0c302b0af334ef22752f3c
andech <7578191@gmail.com>
2020-11-15 10:51:24 +0300

    Integrated endpoint to import CSV files

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,10 +1,10 @@
 const API_PATHS = {
-  product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   products:
-    "https://a5wex8b3kd.execute-api.eu-west-1.amazonaws.com/dev/products",
-  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://a5wex8b3kd.execute-api.eu-west-1.amazonaws.com/dev/products",
+    'https://a5wex8b3kd.execute-api.eu-west-1.amazonaws.com/dev/products',
+  order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://idbcvtwhbl.execute-api.eu-west-1.amazonaws.com/dev',
+  bff: 'https://a5wex8b3kd.execute-api.eu-west-1.amazonaws.com/dev/products',
 };
 
 export default API_PATHS;
